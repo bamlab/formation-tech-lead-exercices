b3ef55a45405889bb7fa935f4c1a3f7707bdf1d7
louiszawadzki <louisz@bam.tech>
2021-10-15 14:26:19 +0200

    Rearchitecture with navigation

--- pages/RNInternals/RNInternals.test.js
+++ pages/RNInternals/RNInternals.test.js
@@ -1,15 +1,10 @@
-/**
- * @format
- */
-
 import React from 'react';
-import App from '../App';
 
-// Note: test renderer must be required after react-native.
 import {render, fireEvent} from '@testing-library/react-native';
+import {RNInternals} from './RNInternals';
 
 it('renders correctly', () => {
-  const {getByText} = render(<App />);
+  const {getByText} = render(<RNInternals />);
 
   const goodButton = getByText('Click to preload https://google.com/toto.png');
   fireEvent.press(goodButton);
